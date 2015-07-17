68091a3457d7e7c4729070fbab1706195dd67686
gittenberg <mjseeger@gmail.com>
2015-07-17 13:27:58 +0200

    Test from PyCharm

--- analysis/plot_performance.py
+++ analysis/plot_performance.py
@@ -6,7 +6,7 @@ Created on 04.11.2014
 import matplotlib.pyplot as plt
 import numpy
 
-mrnas = [6,     60,    600,   6000,   60000]
+mrnas = [6, 60, 600, 6000, 60000]
 times = [0.082, 0.824, 7.664, 83.700, 449.09]
 
 fig, ax = plt.subplots()
@@ -15,6 +15,8 @@ width = 0.35
 plt.bar(ind, times, log=True)
 ax.set_title('Time required to solve timecourses [s]', fontsize=11)
 ax.set_xlabel('number of mRNAs')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( mrnas )
+ax.set_xticks(ind + width)
+ax.set_xticklabels(mrnas)
 plt.show()
+
+# test
