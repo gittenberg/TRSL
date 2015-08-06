29cca9ea075f1a04e20af5142566d4ece85799d0
gittenberg <mjseeger@gmail.com>
2015-08-06 15:31:41 +0200

    Minor

--- translation/TRSL.py
+++ translation/TRSL.py
@@ -54,33 +54,32 @@ class TRSL(object):
 
         # Parameters
         ##################################################################################################################################
-        self.types_tRNA = 42  # number of types of tRNAs
-        V = 4.2e-17  # m^3 # cell volume
-        n_genes = 3795  # number of protein-coding genes in the experiment
-        n_tRNA = 3300000  # number of tRNAs # http://nar.oxfordjournals.org/content/suppl/2011/04/23/gkr300.DC1/Supplemental_File_S2.pdf gives 3000000
+        self.types_tRNA = 42  # number of types of tRNAs, including termination factor
+        V = 4.2e-17           # m^3 # cell volume
+        n_genes = 3795        # number of protein-coding genes in the experiment
+        n_tRNA = 3300000      # number of tRNAs # http://nar.oxfordjournals.org/content/suppl/2011/04/23/gkr300.DC1/Supplemental_File_S2.pdf gives 3000000
 
         avogadro = 6.022e23
-        lambda_tRNA = 1.5e-8  # m # characteristic length tRNA
-        D_tRNA = 8.42e-11  # m^2/s # diffusion coefficient of tRNA
-        tau_tRNA = lambda_tRNA ** 2 / 6 / D_tRNA  # s # char. time for tRNA
-        num_pos_tRNA = V / lambda_tRNA ** 3  # number of discrete positions for tRNA
-        lambda_ribo = 3e-8  # m # characteristic length tRNA
-        D_ribo = 3e-13  # m^2/s # diffusion coefficient of ribosomes
-        tau_ribo = lambda_ribo ** 2 / 6 / D_ribo  # s # char. time for ribosomes
-        num_pos_ribo = V / lambda_ribo ** 3  # number of discrete positions for ribosomes
-        p_init = math.sqrt(3.5e-6 * 0.115)  # initiation probability at mRNA 5' end # we choose the geometric mean btw the lowest and highest possible value
-        competition = 7.78e-4  # tRNA competition coefficient
+        lambda_tRNA = 1.5e-8                       # m # characteristic length tRNA
+        D_tRNA = 8.42e-11                          # m^2/s # diffusion coefficient of tRNA
+        tau_tRNA = lambda_tRNA ** 2 / 6. / D_tRNA  # s # char. time for tRNA
+        num_pos_tRNA = V / lambda_tRNA ** 3        # number of discrete positions for tRNA
+        lambda_ribo = 3e-8                         # m # characteristic length tRNA
+        D_ribo = 3e-13                             # m^2/s # diffusion coefficient of ribosomes
+        tau_ribo = lambda_ribo ** 2 / 6. / D_ribo  # s # char. time for ribosomes
+        num_pos_ribo = V / lambda_ribo ** 3        # number of discrete positions for ribosomes
+        p_init = math.sqrt(3.5e-6 * 0.115)         # initiation probability at mRNA 5' end # we choose the geometric mean btw the lowest and highest possible value
+        competition = 7.78e-4                      # tRNA competition coefficient
 
         # Initial values
         ##################################################################################################################################
-        self.n_mRNA = 6  # 60000 # number of mRNAs
-        self.ribo_free = nribo  # 200000; number of ribosomes not bound to mRNA # http://bionumbers.hms.harvard.edu/search.aspx?task=searchbytrmorg&log=y&unt=y&trm=ribosomes/cell&org=%
+        self.n_mRNA = 60000            # 60000 # number of mRNAs
+        self.ribo_free = nribo         # 200000; number of ribosomes not bound to mRNA # http://bionumbers.hms.harvard.edu/search.aspx?task=searchbytrmorg&log=y&unt=y&trm=ribosomes/cell&org=%
 
         self.GTP = 1e2 * avogadro * V  # GTP molecules (made up)
-        self.GDP = 0  # GDP molecules
+        self.GDP = 0                   # GDP molecules
         self.ATP = 1e2 * avogadro * V  # ATP molecules (made up)
-        self.AMP = 0  # AMP molecules
-        # self.AA = 1e2*avogadro*V          # amino acids (made up)
+        self.AMP = 0                   # AMP molecules
 
         self.timerange = []
         self.timecourses = {}
