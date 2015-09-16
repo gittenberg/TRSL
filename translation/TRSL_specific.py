74fcbb33839216573f7053747b2e8f18ee90849d
gittenberg <mjseeger@gmail.com>
2015-09-16 17:49:54 +0200

    Ribosome density (work in progress)

--- translation/TRSL_specific.py
+++ translation/TRSL_specific.py
@@ -187,7 +187,7 @@ wobble = {codon: 1.000 if codon_anticodon[codon] is not '*' and codon == revcom(
 
 class TRSL_spec(TRSL.TRSL):
     def __init__(self, mRNAs, gene_library, decay_constants=None, nribo=200000, proteome=col.Counter({}), detail=False):
-        super(TRSL_spec, self).__init__(nribo, proteome,detail)
+        super(TRSL_spec, self).__init__(nribo, proteome, detail)
         self._tRNA = col.Counter({i: tRNA_types[i]['abundancy'] for i in tRNA_types})
         self.mRNAs = mRNAs
         self.n_mRNA = len(self.mRNAs)
