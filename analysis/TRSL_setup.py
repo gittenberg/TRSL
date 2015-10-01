afddc03bcf9cc3a9d52e686b0b2d145750a703e7
gittenberg <mjseeger@gmail.com>
2015-10-01 10:40:30 +0200

    Created new run setup (updated transcriptome and init rates)

--- analysis/TRSL_setup.py
+++ analysis/TRSL_setup.py
@@ -65,11 +65,17 @@ conf[7] = {
            'init_rates': pkl.load(open("../parameters/init_rates_plotkin_old.p", "rb")),
            'description': 'full transcriptome and exome, no decay, old (buggy) initiation rates according to Plotkin'
            }
+conf[8] = {
+           'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_shah.p", "rb")),
+           'init_rates': pkl.load(open("../parameters/init_rates_plotkin.p", "rb")),
+           'description': 'updated Shah transcriptome, full exome, no decay, updated initiation rates according to Shah'
+           }
 
 if __name__ == "__main__":
     log.basicConfig(level=log.DEBUG, format='%(message)s', stream=sys.stdout)
 
-    for i in [4]:  # set configuration_id
+    for i in [8]:  # set configuration_id
         if 'decay_constants' in conf[i]:
             genes = list(set(conf[i]['exome']) & set(conf[i]['transcriptome']) & set(conf[i]['init_rates']) & set(conf[i]['decay_constants']))
         else:
