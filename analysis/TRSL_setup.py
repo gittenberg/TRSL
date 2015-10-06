3168158a872bc2ab0b1a38de7dc6001c7c756c85
gittenberg <mjseeger@gmail.com>
2015-10-06 13:22:05 +0200

    Push from new directory (old one stopped working)

--- analysis/TRSL_setup.py
+++ analysis/TRSL_setup.py
@@ -27,22 +27,27 @@ conf[1] = {
            'init_rates': {1: 0.1, 2: 0.1},
            'description': 'test configuration with 2 genes in 3 transcripts'
            }
-conf[2] = {}
+conf[2] = {
+           'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_plotkin_20000.p", "rb")),
+           'init_rates': pkl.load(open("../parameters/init_rates_plotkin.p", "rb")),
+           'description': '20000 transcriptome, full exome, no decay, Plotkin initiation probabilities'
+          }
 conf[3] = {
            'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
-           'transcriptome': pkl.load(open("../parameters/transcriptome.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_plotkin.p", "rb")),
            'init_rates': {gene: 8.2e-07 for gene in pkl.load(open("../parameters/init_rates_plotkin.p", "rb"))},  # average initiation rate
            'description': 'full transcriptome and exome, no decay, constant initiation rates'
            }
 conf[4] = {
            'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
-           'transcriptome': pkl.load(open("../parameters/transcriptome.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_plotkin.p", "rb")),
            'init_rates': pkl.load(open("../parameters/init_rates_plotkin.p", "rb")),
            'description': 'full transcriptome and exome, no decay, specific best estimate initiation rates according to Plotkin'
            }
 conf[5] = {
            'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
-           'transcriptome': pkl.load(open("../parameters/transcriptome.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_plotkin.p", "rb")),
            'init_rates': pkl.load(open("../parameters/init_rates_plotkin.p", "rb")),
            'decay_constants': pkl.load(open("../parameters/decay_constants.p", "rb")),
            'description': 'full transcriptome and exome, with decay, specific best estimate initiation rates according to Plotkin'
@@ -54,52 +59,71 @@ conf[6] = {
            'decay_constants': pkl.load(open("../parameters/decay_constants.p", "rb")),
            'description': 'full transcriptome and exome, specific best estimate initiation rates according to Stansfield'
            }
+conf[7] = {
+           'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_plotkin.p", "rb")),
+           'init_rates': pkl.load(open("../parameters/init_rates_plotkin_old_1.p", "rb")),
+           'description': 'full transcriptome and exome, no decay, old (buggy) initiation rates according to Plotkin'
+           }
+conf[8] = {
+           'exome': pkl.load(open("../parameters/orf_coding.p", "rb")),
+           'transcriptome': pkl.load(open("../parameters/transcriptome_shah.p", "rb")),
+           'init_rates': pkl.load(open("../parameters/init_rates_plotkin.p", "rb")),
+           'description': 'updated Shah transcriptome, full exome, no decay, updated initiation rates according to Shah'
+           }
 
-for i in [4]:  # set configuration_id
-    if 'decay_constants' in conf[i]:
-        genes = list(set(conf[i]['exome']) & set(conf[i]['transcriptome']) & set(conf[i]['init_rates']) & set(conf[i]['decay_constants']))
-    else:
-        genes = list(set(conf[i]['exome']) & set(conf[i]['transcriptome']) & set(conf[i]['init_rates']))
-        conf[i]['decay_constants'] = None
-    print "found %s genes in common." % len(genes)
+if __name__ == "__main__":
+    log.basicConfig(level=log.DEBUG, format='%(message)s', stream=sys.stdout)
 
-    mRNAs = []
-    counter = 0
-    for gene in genes:
-        if conf[i]['init_rates']:
-            if gene in conf[i]['transcriptome'] and gene in conf[i]['init_rates']:
-                # print "abundancies and initiation rates available for gene:", gene
-                for instance in range(conf[i]['transcriptome'][gene]):
-                    mRNAs.append(MRNA_specific.mRNA_spec(index=counter, sequence=conf[i]['exome'][gene], geneID=gene, ribosomes={}, init_rate=conf[i]['init_rates'][gene]))  # do not just multiply the list
-                    counter += 1
+    for i in [8]:  # set configuration_id
+        if 'decay_constants' in conf[i]:
+            genes = list(set(conf[i]['exome']) & set(conf[i]['transcriptome']) & set(conf[i]['init_rates']) & set(conf[i]['decay_constants']))
         else:
-            if gene in conf[i]['transcriptome']:
-                print "abundancies but no initiation rate available for gene:", gene
-                for instance in range(conf[i]['transcriptome'][gene]):
-                    mRNAs.append(MRNA_specific.mRNA_spec(index=counter, sequence=conf[i]['exome'][gene], geneID=gene, ribosomes={}))  # do not just multiply the list
-                    counter += 1
-    print "built gene library, next: run TRSL_spec."
+            genes = list(set(conf[i]['exome']) & set(conf[i]['transcriptome']) & set(conf[i]['init_rates']))
+            conf[i]['decay_constants'] = None
+        print "found %s genes in common." % len(genes)
 
-    description = conf[i]['description']
-    print description
+        mRNAs = []
+        counter = 0
+        for gene in genes:
+            if conf[i]['init_rates']:
+                if gene in conf[i]['transcriptome'] and gene in conf[i]['init_rates']:
+                    # print "abundancies and initiation rates available for gene:", gene
+                    for instance in range(conf[i]['transcriptome'][gene]):
+                        mRNAs.append(MRNA_specific.mRNA_spec(index=counter, sequence=conf[i]['exome'][gene], geneID=gene, ribosomes={}, init_rate=conf[i]['init_rates'][gene]))  # do not just multiply the list
+                        counter += 1
+            else:
+                if gene in conf[i]['transcriptome']:
+                    print "abundancies but no initiation rate available for gene:", gene
+                    for instance in range(conf[i]['transcriptome'][gene]):
+                        mRNAs.append(MRNA_specific.mRNA_spec(index=counter, sequence=conf[i]['exome'][gene], geneID=gene, ribosomes={}))  # do not just multiply the list
+                        counter += 1
+        print "built gene library, next: run TRSL_spec."
 
-    duration = 120.0
+        description = conf[i]['description']
+        print description
 
-    tr = TRSL_specific.TRSL_spec(mRNAs, conf[i]['exome'], conf[i]['decay_constants'])
+        duration = 1800.0
 
-    tr._tRNA = col.Counter({i: TRSL_specific.tRNA_types[i]['abundancy'] for i in TRSL_specific.tRNA_types})
-    tr._tRNA_free = col.Counter({i: int(tr._tRNA[i]) for i in TRSL_specific.tRNA_types})  # tRNA not bound to ribosomes
-    tr._tRNA_bound = tr._tRNA - tr._tRNA_free  # tRNA bound to ribosomes
-    tr.solve_internal(0.0, duration, deltat=1.0)
+        tr = TRSL_specific.TRSL_spec(mRNAs, conf[i]['exome'], conf[i]['decay_constants'], nribo=200000, detail=True)
 
-    '''
-    # Profiling:
-    cProfile.run('tr.solve_internal(0.0, '+str(duration)+', deltat=1.0)', 'trsl_profile')
-    p=pstats.Stats('trsl_profile')
-    p.strip_dirs().sort_stats('cumulative').print_stats()
-    #tr.inspect()
-    '''
+        tr._tRNA = col.Counter({i: TRSL_specific.tRNA_types[i]['abundancy'] for i in TRSL_specific.tRNA_types})
+        tr._tRNA_free = col.Counter({i: int(tr._tRNA[i]) for i in TRSL_specific.tRNA_types})  # tRNA not bound to ribosomes
+        tr._tRNA_bound = tr._tRNA - tr._tRNA_free  # tRNA bound to ribosomes
+        #tr.solve_internal(0.0, duration, deltat=1.0)
 
-    tr.dump_results(description)
+        # Profiling:
+        import cProfile
+        cProfile.run('tr.solve_internal(0.0, '+str(duration)+', deltat=0.1)', 'trsl_profile')
+        import pstats
+        p=pstats.Stats('trsl_profile')
+        p.strip_dirs().sort_stats('cumulative').print_stats()
+        #tr.inspect()
 
-print "done."
+        tr.dump_results(description)
+
+        # write last polysomes to shelve database
+        tr.write_last_polysome(description)
+
+
+    print "done."
