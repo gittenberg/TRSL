f35e4f55d3706b525719a84e370df4ac9bff554e
gittenberg <mjseeger@gmail.com>
2016-04-14 20:49:24 +0200

    Minor

--- analysis/TRSL_regression_against_translation_rates.R
+++ analysis/TRSL_regression_against_translation_rates.R
@@ -5,11 +5,13 @@ data = read.csv("regression_data.csv", sep=",")
 # rename columns
 colnames(data) = c("X", "gene", "translation.rate", "transcript.abundance", "initiation.rate", "ORF.length", "CAI")
 
-# 
-lm(data$translation.rate ~ data$initiation.rate + data$CAI)
+# unnormalized model
+model_unnorm = lm(data$translation.rate ~ data$initiation.rate + data$CAI)
+summary(model_unnorm)
 
 IR_norm = scale(data$initiation.rate)
 CAI_norm = scale(data$CAI)
 
+# normalized model
 model = lm(data$translation.rate ~ IR_norm + CAI_norm)
 summary(model)
