c7de948ddcd4a1bdbea36cdc25a76aa22cb55f64
gittenberg <mjseeger@gmail.com>
2016-04-27 17:59:13 +0200

    Evaluating impact of ribosome number

---
+++ analysis/TRSL_vary_ribosomes.py
@@ -0,0 +1,74 @@
+'''
+Script to simulate a grid of the following conditions:
+
+- transcriptomes estimated at S phase and scaled down by a factor of 2
+- different numbers of ribosomes
+
+to test the hypothesis of an optimal translational efficiency
+
+Created on 27.04.2016
+
+@author: MJS
+'''
+print "TRSL_vary_ribosomes: starting"
+import logging as log
+import sys
+import cPickle as pkl
+import collections as col
+
+from translation import MRNA_specific, TRSL_specific
+
+log.basicConfig(level=log.DEBUG, format='%(message)s', stream=sys.stdout)
+
+# load the two transcriptomes
+transcriptomes_dict = pkl.load((open('../parameters/transcriptome_time_dependent.p')))
+transcriptome_scaled = {key: int(round(transcriptomes_dict[25][key]*0.5, ndigits=0)) for key in transcriptomes_dict[25]}  # S phase is at 25 minutes
+transcriptomes_dict = {'S': transcriptomes_dict[25], 'S_scaled': transcriptome_scaled}
+
+for transcriptome in transcriptomes_dict:
+    print "found {} transcripts in transcriptome {}...".format(sum(transcriptomes_dict[transcriptome].values()), transcriptome)
+
+# load other data
+exome = pkl.load(open("../parameters/orf_coding.p", "rb"))
+init_rates = pkl.load(open("../parameters/init_rates_enhanced_median.p", "rb"))  # missing replaced by median
+
+duration = 1200.0  # should be sufficent to saturate
+ribonumbers = range(50000, 500000, 50000)
+
+for ribonumber in ribonumbers:
+    for phase_ID in transcriptomes_dict:
+        mRNAs = []
+        description = '{} ribosomes, {} phase transcriptome, full exome, no decay, median-enhanced initiation rates according to Shah'.format(ribonumber, phase_ID)
+
+        counter = 0
+        genes = list(set(exome) & set(transcriptomes_dict[phase_ID]) & set(init_rates))
+        print "found %s genes in common." % len(genes)
+
+        for gene in genes:
+            for transcript in range(transcriptomes_dict[phase_ID][gene]):
+                mRNAs.append(MRNA_specific.mRNA_spec(index=counter, sequence=exome[gene], geneID=gene, ribosomes={}, init_rate=init_rates[gene]))  # do not just multiply the list
+                counter += 1
+
+        print "created transcriptome: {}.".format(description)
+
+        tr = TRSL_specific.TRSL_spec(mRNAs, exome, decay_constants=None, nribo=ribonumber, proteome=col.Counter({}), detail=False)
+
+        # tr._tRNA = col.Counter({i: TRSL_specific.tRNA_types[i]['abundancy'] for i in TRSL_specific.tRNA_types})
+        tr._tRNA = col.Counter({i: int(TRSL_specific.tRNA_types[i]['abundancy'] * len(genes) / len(exome)) for i in TRSL_specific.tRNA_types})  # we do not let tRNA vary like the ribosomes
+        tr._tRNA_free = col.Counter({i: int(tr._tRNA[i]) for i in TRSL_specific.tRNA_types})  # tRNA not bound to ribosomes
+        tr._tRNA_bound = tr._tRNA - tr._tRNA_free  # tRNA bound to ribosomes
+
+        # Run without profiling:
+        # tr.solve_internal(0, duration, deltat=0.2)
+
+        # Run with profiling:
+        import cProfile
+        cProfile.run('tr.solve_internal(0, '+str(duration)+', deltat=0.2)', 'trsl_profile')
+        import pstats
+        p = pstats.Stats('trsl_profile')
+        p.strip_dirs().sort_stats('cumulative').print_stats()
+
+        tr.dump_results(description)
+
+
+
