d1c2ec15c799aca90328e2fec48668ab8ce158e8
gittenberg <mjseeger@gmail.com>
2017-10-06 16:49:45 +0200

    yeast tAIs from R to Python (but inconsistent!)

---
+++ workbooks/analyses/09 Codon_usage_analysis.R
@@ -0,0 +1,30 @@
+# script adapted from https://github.com/mariodosreis/tai
+
+#setwd("~/tai-master/misc")
+setwd("~/git/TRSL/workbooks/analyses/tAI_misc")
+
+require("tAI")
+yeast.trna = scan("yeast.trna")
+yeast.ws = get.ws(tRNA=yeast.trna, sking=0) # superkingdom, 0 indicating Eukaryota
+yeast.m = matrix(scan("yeast.m"), ncol=61, byrow=TRUE)
+yeast.m = yeast.m[,-33]
+
+yeast.tai = get.tai(yeast.m, yeast.ws)
+hist(yeast.tai)
+
+# annotate tAI by name
+
+#biocLite("Biostrings")  # to install Biostrings
+library(Biostrings)
+dna = readDNAStringSet("orf_coding.fasta")
+
+# get names only (from https://stackoverflow.com/questions/20428742/select-first-element-of-nested-list)
+s = strsplit(names(dna), "[ \t]+") # split names by tab/space
+gene.names = lapply(s, function(l) l[[1]])
+
+# name the list (https://stackoverflow.com/questions/17842705/creating-a-named-list-from-two-vectors-names-values)
+names(yeast.tai) = gene.names
+
+library(RJSONIO)
+yeast.json = toJSON(list(yeast.tai)[[1]])
+write(yeast.json, file="yeast.json")
